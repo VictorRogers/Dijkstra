9732efdbafa8143e0092523ebd0bb763b6273dbe
Vic <victor.r.rogers@gmail.com>
2014-11-16 16:06:27 -0600

    Create Node.cpp

---
+++ Node.cpp
@@ -0,0 +1,34 @@
+#include "Node.h"
+
+
+//=============================================================================
+// Class: Node
+// Function: Default Constructor
+// Parameters: None
+// Description: 
+// TODO: Revisit after implementing the graph.
+//=============================================================================
+Node::Node() : priority(0) {}
+//=============================================================================
+
+
+//=============================================================================
+// Class: Node
+// Function: Overloaded Constructor
+// Parameters: (int priority)
+// Description: 
+// TODO: Revisit after implementing the graph.
+//=============================================================================
+Node::Node(int priority) : priority(priority) {}
+//=============================================================================
+
+
+//=============================================================================
+// Class: Node
+// Function: Destructor
+// Parameters: None
+// Description: 
+// TODO: Revisit after implementing the graph.
+//=============================================================================
+Node::~Node() {}
+//=============================================================================
